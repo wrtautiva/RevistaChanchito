39a9ab6dbff150717a82349748c718d06f173d45
Kurosou <43392610+Kurosou@users.noreply.github.com>
2020-09-27 14:54:43 -0500

    Front v0.1
    Correccion del front, adicion logo

---
+++ chanchitoFront/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+//ROOT
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'chanchitoFront';
+}
---
+++ chanchitoFront/src/app/grid-list/grid-list.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-list',
+  templateUrl: './grid-list.component.html',
+  styleUrls: ['./grid-list.component.css']
+})
+export class GridListComponent{}
+
