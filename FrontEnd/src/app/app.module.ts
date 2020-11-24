6d81917bfb7739abba3ae762bd82fc96676750a8
odrugees <odrugees@poligran.edu.co>
2020-11-23 19:05:44 -0500

    Ajuste menu y componentes
    Co-Authored-By: null <odrugees@users.noreply.github.com>

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,33 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutModule } from './@comoponents/about/about.module';
-import { ContactModule } from './@comoponents/contact/contact.module';
-import { CopyrightModule } from './@comoponents/copyright/copyright.module';
-import { FooterModule } from './@comoponents/footer/footer.module';
-import { HeaderModule } from './@comoponents/header/header.module';
-import { NavbarModule } from './@comoponents/navbar/navbar.module';
+import { AboutComponent } from './@comoponents/about/about.component';
+import { ContactComponent } from './@comoponents/contact/contact.component';
+import { CopyrightComponent } from './@comoponents/copyright/copyright.component';
+import { FooterComponent } from './@comoponents/footer/footer.component';
+import { HeaderComponent } from './@comoponents/header/header.component';
+import { NavbarComponent } from './@comoponents/navbar/navbar.component';
 import { PortafolioModule } from './@comoponents/portafolio/portafolio.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthorComponent} from './@comoponents/author/author.component';
+import { ContextComponent} from './@comoponents/context/context.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/header', pathMatch:'full'},
+  {path: 'header', component: HeaderComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'author', component: AuthorComponent},
+  {path: 'context', component: ContextComponent},
+
+
+]
 
 @NgModule({
-  declarations: [ 
+  declarations: [
     AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    CopyrightComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AboutModule,
-    ContactModule,
-    CopyrightModule,
-    FooterModule,
-    HeaderModule,
-    NavbarModule,
-    PortafolioModule,
-    NgbModule
-    
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
